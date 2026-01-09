da796d94c8766cb4613ba288f137ed59c069fddb
Hernan Lencinas <phlencinas@personal.com.ar>
2026-01-09 17:42:21 -0300

    FIX Se elimina elementos no necesarios

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ToastPrimitives from "@radix-ui/react-toast"
 import { cva, type VariantProps } from "class-variance-authority"
-import { X, CheckCircle2, XCircle, Info, AlertTriangle } from "lucide-react"
+import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -44,7 +44,7 @@ const toastVariants = cva(
 const Toast = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof ToastPrimitives.Root> &
-    VariantProps<typeof toastVariants>
+  VariantProps<typeof toastVariants>
 >(({ className, variant, ...props }, ref) => {
   return (
     <ToastPrimitives.Root
